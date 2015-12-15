4db09306faa0ac679866aa9dd8aa50544826c4e1
MahdiMahava <avbondarik@gmail.com>
2015-12-15 20:03:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/dbrequest', function(req, res)
   
 
  
-      res.send("Params = "+req + "<hr/> originalUrl = " + req.originalUrl + " <hr/> params = " req.params + "<hr/ baseUrl = >"+req.baseUrl + "<hr/> query = "+ req.query.q);
+      res.send( "<hr/> query = " + req.query.q);
 
 
 });
