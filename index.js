08f2140b705d2c2047cddf271543b43f1614335f
MahdiMahava <avbondarik@gmail.com>
2015-12-15 16:11:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 var app = require('express').createServer();
 app.get('/', function(req, res) {
   res.send("<h1> Hello Anton!!!! </h1>");
+  
+  
+  var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+      res.send(JSON.stringify(row));
+    });
+});
+
+
+
 });
 
 app.listen(process.env.PORT || 3000, function() {
