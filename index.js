4d55321fc7f204cb0b383a3a5a713a16d83076d6
MahdiMahava <avbondarik@gmail.com>
2015-12-15 15:33:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var app = require('express').createServer();
+app.get('/', function(req, res) {
+  res.send("Hello World");
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(process.env.PORT || 3000, function() {
+  console.log("listening on 3000");
 });
